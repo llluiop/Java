c9876b3155c54b23cb687d0094b526358a91c784
Yu Liu <yu.l@fetchr.us>
2019-04-14 10:50:26 +0800

    add cp count

--- src/com/L/HelloWorld.java
+++ src/com/L/HelloWorld.java
@@ -2,6 +2,12 @@ package com.L;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello");
+        String s = "你好\uD835\uDD46";
+        int len = s.length();
+        int cpCount = s.codePointCount(0, s.length());
+        System.out.println(s);
+        System.out.println(len);
+        System.out.println(cpCount);
+
     }
 }
---
+++ src/com/Y/AccessControl.java
@@ -0,0 +1,19 @@
+package com.Y;
+
+import com.L.HelloWorld;
+
+public class AccessControl {
+    HelloWorld hello;
+    AccessControl() {
+        hello = new HelloWorld();
+    }
+
+    void AccessCtrl() {
+        //int i1 = hello.protected_mem;
+        //int i2 = hello.private_mem;
+        //int i3 = hello.public_mem;
+    }
+    void Say(){
+        System.out.println("hello 2");
+    }
+}
